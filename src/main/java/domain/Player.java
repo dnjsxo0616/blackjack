8a7cfcdf91864ebe2c914e18b7d0f2eea14c1fa5
KDT live <dnjsxo0616@gmail.com>
2023-11-30 01:30:30 +0900

    feat: player 초기 변수 추가

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,14 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+
+    private List<Integer> playerCardBox;
+    private int playerNumber;
+
+    public Player() {
+        this.playerCardBox = new ArrayList<>();
+    }
+}
