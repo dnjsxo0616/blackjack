4e5c0a308fd74a5e5ec705c5f68ae09830fba2ea
KDT live <dnjsxo0616@gmail.com>
2023-11-30 22:02:39 +0900

    test: 입력 오류 테스트

--- src/test/java/domain/ValidateInputTest.java
+++ src/test/java/domain/ValidateInputTest.java
@@ -1,12 +1,12 @@
 package domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidateInputTest {
-
     @Test
     @DisplayName("대소문자_입력_테스트")
     void validateInput() {
@@ -18,5 +18,15 @@ class ValidateInputTest {
 
         assertEquals("y", ValidateInput.validateInput(input1.toUpperCase()));
         assertEquals("n", ValidateInput.validateInput(input2.toUpperCase()));
+    }
+
+    @Test
+    @DisplayName("입력_오류_테스트")
+    void validateInputFail() {
+        String input = "x";
+
+        Assertions.assertThatThrownBy(()-> ValidateInput.validateInput(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못 입력하셨습니다.");
     }
 }
