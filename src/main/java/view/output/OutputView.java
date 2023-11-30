945fe3cce46f93982934b91cfa7189945fecb2db
KDT live <dnjsxo0616@gmail.com>
2023-11-30 16:00:38 +0900

    refactor: println -> print 및 빈칸 출력 기능 추가

--- src/main/java/view/output/OutputView.java
+++ src/main/java/view/output/OutputView.java
@@ -3,8 +3,12 @@ package view.output;
 import view.contents.ContentsMessage;
 
 public class OutputView {
-    public static void println(ContentsMessage contentsMessage) {
-        System.out.println(contentsMessage.getMessage());
+    public static void print(ContentsMessage contentsMessage) {
+        System.out.print(contentsMessage.getMessage());
+    }
+
+    public static void printNewLine() {
+        System.out.println();
     }
 
     public static void println(ContentsMessage contentsMessage, int number) {
