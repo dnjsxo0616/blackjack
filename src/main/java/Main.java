c7344ba1de65f8e215cee084e98dc1eef73379d6
KDT live <dnjsxo0616@gmail.com>
2023-11-30 17:01:28 +0900

    feat: 실행 기능 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import controller.GameController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GameController gameController = new GameController();
+        gameController.GamePlay();
     }
 }
