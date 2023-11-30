62199a75a8b7a541ce935a7a69783e19aff6468a
KDT live <dnjsxo0616@gmail.com>
2023-11-30 16:46:13 +0900

    refactor: void -> String 반환

--- src/main/java/domain/ValidateInput.java
+++ src/main/java/domain/ValidateInput.java
@@ -3,11 +3,13 @@ package domain;
 public class ValidateInput {
     private static final String RESTART_COMMAND = "y";
     private static final String STOP_COMMAND = "n";
-    public static void validateInput(String input) {
+    public static String validateInput(String input) {
         String command = input.toLowerCase();
 
         if(!command.equals(RESTART_COMMAND) && !command.equals(STOP_COMMAND)) {
             throw new IllegalArgumentException("잘못 입력하셨습니다.");
         }
+
+        return command;
     }
 }
